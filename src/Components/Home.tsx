b67bbeaf23caabf9db29dafdbd90578d8f317092
stbn1699 <stbnbasson@gmail.com>
2023-07-18 15:09:48 +0200

    feat home connexion

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,16 +1,21 @@
 // @ts-ignore
 import ButtonCustom from "/src/Components/utilities/ButtonCustom";
 import "@/styles/styles.css"
-import {useState} from 'react'
+import {useId, useState} from 'react'
 import {Link, useNavigate} from "react-router-dom"
+import ConnexionForm from "@/Components/utilities/ConnexionForm";
 
 
 function Home() {
 
+    const username = useId()
+    const passwrd = useId()
+
     return (
-        <div className="welcomeScreen">
+        <div className="welcomeScreen whitesmoke">
             <h1 className="welcomeTitle">Bienvenue !</h1>
-            <ButtonCustom title="Se Connecter" buttontype="1" path="connexion"/>
+            <h3 className="welcomeSubtitle">Connectez-vous</h3>
+            <ConnexionForm/>
         </div>
     )
 
